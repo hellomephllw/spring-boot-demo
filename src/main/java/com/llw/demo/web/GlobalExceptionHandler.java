f77c851a82fd09f1daa0e1848f945dd0ccd13205
hellomephllw <hellomephllw@163.com>
2018-11-15 15:38:23 +0800

    加入全局异常处理

---
+++ src/main/java/com/llw/demo/web/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.llw.demo.web;
+
+import com.llw.base.ResultVo;
+import com.llw.exception.BussinessException;
+import com.llw.util.LoggerUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @discription: 全局异常处理器
+ * @author: llw
+ * @date: 2018-11-15
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public ResultVo defaultExceptionHandler(Exception e) {
+
+        logger.error(">>>>>>>>>出现异常");
+
+        if (e instanceof BussinessException) {
+            logger.error(e.getMessage());
+            return new ResultVo<>(0, e.getMessage(), null);
+        } else if (e instanceof MissingServletRequestParameterException) {
+            logger.error(e.getMessage());
+            return new ResultVo<>(0, "缺少参数", null);
+        }
+
+        LoggerUtil.printStackTrace(logger, e);
+
+        return new ResultVo<>(0, "服务器繁忙", null);
+    }
+
+}
--- src/main/java/com/llw/demo/web/controller/UserController.java
+++ src/main/java/com/llw/demo/web/controller/UserController.java
@@ -1,8 +1,8 @@
 package com.llw.demo.web.controller;
 
-import com.llw.exception.BussinessException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @PostMapping("/test")
-    public String test() throws Exception {
+    public String test(@RequestParam String nood) throws Exception {
 
         return "test";
     }
